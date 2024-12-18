0a11c8090397c4eaacb0941c07450bc1d3a3682b
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2024-12-18 16:52:18 +0600

    Fix title and CI

--- app.vue
+++ app.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="flex justify-center bg-background font-zed">
-    <div class="flex min-h-screen w-screen max-w-[60rem] flex-col items-center px-6 pb-10 pt-7 text-[0.92rem] leading-relaxed sm:px-10 lg:px-10">
-      <!-- <Header></Header> -->
-      <NuxtPage />
-      <Footer></Footer>
+    <div class="flex justify-center bg-background font-zed">
+        <div
+            class="flex min-h-screen w-screen max-w-[60rem] flex-col items-center px-6 pb-10 pt-7 text-[0.92rem] leading-relaxed sm:px-10 lg:px-10"
+        >
+            <!-- <Header></Header> -->
+            <NuxtPage />
+            <Footer></Footer>
+        </div>
     </div>
-  </div>
 </template>
 <script setup>
 useHead({
-  bodyAttrs: {
-    class: 'dark'
-  }
-})
+    bodyAttrs: {
+        class: "dark",
+        title: "Tareque Md Hanif",
+    },
+});
 </script>
